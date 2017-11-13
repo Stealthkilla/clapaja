8d16a6986c8d97e290cf96d1d9d9328f120d2faa
Manuel Steiner <EpicStonie@users.noreply.github.com>
2017-11-13 15:19:39 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
-header("Content-Type: text/html; charset=utf-8");
+header("Content-Type: text/html; charset=utf-8; refresh:5; url=index.php");
+
 
 // Datenbank Variablen
-$datenbank = 'citationdb';
-$abfrage = 'SELECT * FROM citation WHERE ID = 1';
+$datenbank = 'educati3_citationdb';
 
 require_once ('inc/db_inc.php');
 
@@ -11,7 +11,16 @@ require_once ('inc/db_inc.php');
 $verbindung = mysqli_connect ($host, $user, $passwd , $datenbank) or die ('Fehler mysqli_connect(): ' . mysqli_error($verbindung));
 mysqli_set_charset($verbindung, "utf8");
 
-// Datenbank-Abfrage
+//	Datanbank abfrage Anzahl Zitate
+//$abfrage = "Count(ID), quote FROM citation AS 'number' ";
+//$res = mysqli_query ($verbindung, $abfrage);
+//$number = mysqli_fetch_assoc($res);
+
+//$Id = rand(1,$number[number]);
+
+
+// Datenbank-Abfrage Zitat abrufen
+$abfrage = "SELECT * FROM citation WHERE ID = 1 ";
 $res = mysqli_query ($verbindung, $abfrage);
 $zitat = mysqli_fetch_assoc($res);
 
@@ -26,15 +35,16 @@ mysqli_close ($verbindung);
 		<meta charset="utf-8">
 		<meta name="description" content="A website who shows various zitate with author">
 		<meta name="language" content="DE">
+		<meta http-equiv="refresh" content="5;url=index.php">
 		
 		<title>Zitate</title>
 		
 		<link href="css/layout.css" type="text/css" rel="Stylesheet" />
 	</head>
 	<body>
-		<div>
-			<h1><?php $zitat[quote] ?></h1>
-			<h3><?php $zitat[author] ?></h3>
+		<div id="zitatbox">
+			<h1><?php echo $zitat[quote] ?></h1>
+			<h3><?php echo $zitat[author] ?></h3>
 		</div>
 	</body>
 </html>
