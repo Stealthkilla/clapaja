1e2553209f9c90ebb052468a4cf2e9c6052fd066
Cathbodua <33619025+Cathbodua@users.noreply.github.com>
2017-11-13 11:07:22 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang ="de-CH">
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="A website who shows various zitate with author">
+		<meta name="language" content="DE">
+		
+		<title>Zitate</title>
+		
+	</head>
+	
+	<body>
+
+	</body>
+</html>
