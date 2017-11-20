478bbece081e399dd6e5111d580baad0ca071877
Stealthkilla <33425107+Stealthkilla@users.noreply.github.com>
2017-11-20 16:02:52 +0100

    PHP Code Removed added JQuery Code
    Auslagerung der Abfrage in data.php und implementierung der Javascript abfrage.

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
-<?php
-
-header("Content-Type: text/html; charset=utf-8; refresh:5; url=index.php");
-
-
-// Datenbank Variablen
-$datenbank = 'educati3_citationdb';
-
-require_once ('inc/db_inc.php');
-
-// Zur Datenbank verbinden
-$verbindung = mysqli_connect ($host, $user, $passwd , $datenbank) or die ('Fehler mysqli_connect(): ' . mysqli_error($verbindung));
-mysqli_set_charset($verbindung, "utf8");
-
-//	Datanbank abfrage Anzahl Zitate
-//$abfrage = "Count(ID), quote FROM citation AS 'number' ";
-//$res = mysqli_query ($verbindung, $abfrage);
-//$number = mysqli_fetch_assoc($res);
-
-//$Id = rand(1,$number[number]);
-
-
-// Datenbank-Abfrage Zitat abrufen
-$abfrage = mysqli_query($verbindung, "SELECT ID, quote, author, birthdate FROM citation ORDER BY RAND() LIMIT 1" );
-$zitat = mysqli_fetch_assoc($abfrage);
-
-// Verbindung trennen
-mysqli_close ($verbindung);
-
-?>
-
 <!DOCTYPE html>
 <html lang ="de-CH">
 	<head>
@@ -37,19 +6,28 @@ mysqli_close ($verbindung);
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="A website who shows various zitate with author">
 		<meta name="language" content="DE">
-		<meta http-equiv="refresh" content="5;url=index.php">
 		<title>Zitate</title>
-		<link href="css/layout.css" type="text/css" rel="stylesheet">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/layout.css" type="text/css" rel="stylesheet">
 	</head>
 	<body>
 		<div class="container">
  			<div class="jumbotron vertical-center">
-    		<h1><?php echo $zitat[quote] ?></h1>
-			<h3><?php echo $zitat[author] ?></h3>
+    		<h1 id="quote"></h1>
+			<p id="author"></p>
+			<p id="date"></p>
   			</div>
   		</div>
-  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  		<script src="js/bootstrap.min.js"></script>
+  
 	</body>
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+	<script>
+		$.getJSON( "data.php", function(json) {
+  			console.log( "JSON Data: " + json );
+  			$("#quote").text(json.quote);
+  			$("#author").text(json.aname);
+  			$("#date").text(json.abirthdate);
+		 });
+ 	</script>
 </html>
