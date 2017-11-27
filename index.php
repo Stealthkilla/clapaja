596d1c954255ef9208aef3bfb9a2a9841141a018
Stealthkilla <33425107+Stealthkilla@users.noreply.github.com>
2017-11-27 14:01:18 +0100

    Few changes in librarys

--- index.php
+++ index.php
@@ -7,12 +7,9 @@
 		<meta name="language" content="DE">
 		<title>Zitate</title>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link href="css/layout.css" type="text/css" rel="stylesheet">
-		
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body id="backgroundId">
 		<nav class="navbar navbar-default">
@@ -22,16 +19,16 @@
     			</div>
 				<ul class="nav navbar-nav">
 					<li class="active"><a href="#">Home</a></li>
-					<li><a href="#">Page 1</a></li>
-					<li><a href="#">Page 2</a></li>
-					<li><a href="#">Page 3</a></li>
+					<li><a href="login.php">Login</a></li>
 				</ul>
 			</div>
 		</nav>
 		<!-- Beginn of Jumbotron -->
 		<div class="container">
  			<div class="jumbotron vertical-center" id="messageBlock">
-    		<h1 id="quote"></h1>
+    			<div class="page-header">
+  				<h1 id="quote"></h1>
+				</div>
 			<p id="author"></p>
 			<p id="date"></p>
   			</div>
